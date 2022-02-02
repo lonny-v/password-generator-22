05904419ec8d7c74cf45c12297b9aa6d38c0de64
Lonny Vberg <lonnyvandenberg@gmail.com>
2022-02-01 22:50:57 -0700

    functional except for randomized order

--- Develop/script.js
+++ Develop/script.js
@@ -18,74 +18,83 @@
 
 // display generated password in textbox after all prompts are answered
 
+
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  
-  document.getElementById("password").innerHTML=length;
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-var length = prompt("Choose a password length between 8 and 128 characters")
-var includeLower = confirm("include lowercase letters?");
-var includeUpper = confirm("include uppercase letters?");
-var includeNumber = confirm("include numbers?");
-var includeSymbol = confirm("include special characters?");
-  
-if (!includeLower && !includeUpper && !includeNumbers && !includeSymbols) {
-  return "Please select at least one option"
-}
-  else{
-    var counter = 0;
-    var password = '';
+function generatePassword() {
 
-    while (counter < length) {
-      if (includeLower && counter <= length){
-        password += getRandLower()
-        counter ++
+  var length = parseInt(prompt("Choose a password length between 8 and 128 characters"));
+
+  if (length < 8 || length > 128) {
+    return "Please enter a valid number";
+  }
+  else {
+    var includeLower = confirm("Include lowercase letters?");
+    var includeUpper = confirm("Include uppercase letters?");
+    var includeNumber = confirm("Include numbers?");
+    var includeSymbol = confirm("Include special characters?");
+
+    if (!includeLower && !includeUpper && !includeNumbers && !includeSymbols) {
+      return "Please select at least one option";
+    }
+    else {
+      var counter = 0;
+      var password = '';
+
+      while (counter < length) {
+        if (includeLower && counter <= length) {
+          password += getRandLower() // add random lowercase letter to password
+          counter++ //increase generated password length up to this point by 1
+        }
+        if (includeUpper && counter <= length) {
+          password += getRandUpper()
+          counter++
+        }
+        if (includeNumber && counter <= length) {
+          password += getRandNumber()
+          counter++
+        }
+        if (includeSymbol && counter <= length) {
+          password += getRandSymbol()
+          counter++
+        }
       }
-      if (includeUpper && counter <= length){
-        password += getRandUpper()
-        counter ++
-      }
-      if (includeNumber && counter <= length){
-        password += getRandNumber()
-        counter ++
-      }
-      if (includeSymbol && counter <= length){
-        password += getRandSymbol()
-        counter ++
-      }
+      return password
     }
   }
+}
 
-  
-
-function getRandLower(){
+//Get random i of arrays
+function getRandLower() {
   const lower = 'abcdefghijklmnopqrstuvwxyz';
   return lower[Math.floor(Math.random() * lower.length)];
 }
 
-function getRandUpper(){
+function getRandUpper() {
   const upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   return upper[Math.floor(Math.random() * upper.length)];
 }
 
-function getRandNumber(){
+function getRandNumber() {
   const number = '0123456789';
   return number[Math.floor(Math.random() * number.length)];
 }
 
-function getRandSymbol(){
-  const symbol = ' !@#$%^&*(){}[]=<>/,.';
+function getRandSymbol() {
+  const symbol = '!@#$%^&*(){}[]=<>/,.';
   return symbol[Math.floor(Math.random() * symbol.length)];
 }
