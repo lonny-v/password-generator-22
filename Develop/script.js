1d75b701eb7538426d8b716743e6ff4f68ad79a2
Lonny Vberg <lonnyvandenberg@gmail.com>
2022-02-01 21:21:31 -0700

    added arrays and function to get random [i]

--- Develop/script.js
+++ Develop/script.js
@@ -18,18 +18,54 @@
 
 // display generated password in textbox after all prompts are answered
 
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  var length = prompt("Choose a password length between 8 and 128 characters")
+  prompt("Choose a password length between 8 and 128 characters");
+  confirm("include lowercase letters?");
+  confirm("include uppercase letters?");
+  confirm("include numbers?");
+  confirm("include special characters?");
+
+
+
+  document.getElementById("password").innerHTML=length;
+  // var password = generatePassword();
+  // var passwordText = document.querySelector("#password");
+
+  // passwordText.value = password;
 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+  
+  
+  
+  
+
+function getRandLower(){
+  const lower = 'abcdefghijklmnopqrstuvwxyz';
+  return lower[Math.floor(Math.random() * lower.length)];
+}
+
+function getRandUpper(){
+  const upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  return upper[Math.floor(Math.random() * upper.length)];
+}
+
+function getRandNumber(){
+  const number = '0123456789';
+  return number[Math.floor(Math.random() * number.length)];
+}
+
+function getRandSymbol(){
+  const symbol = ' !@#$%^&*(){}[]=<>/,.';
+  return symbol[Math.floor(Math.random() * symbol.length)];
+}
